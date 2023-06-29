742c17d19c76c73d8a2ac0b0c6757dcb386d96d0
Eduardo Marques <112179103+poporybr@users.noreply.github.com>
2023-06-29 16:13:25 -0300

    media 375px

--- src/components/sectionMain/sectionLeft/sectionLeft.js
+++ src/components/sectionMain/sectionLeft/sectionLeft.js
@@ -6,14 +6,14 @@ export function Left() {
     <div className="containerLeft">
       <div className="containerLeft2">
         <pre>
-          <h1 className="titleLeft">
+          <h1 className="titleLeft titleLeft2">
             Alavanque seu{"\n"}negócio além do{"\n"}físico, conquiste o{"\n"}
             mundo digital
           </h1>
         </pre>
         <pre>
           <p className="paragLeft">
-            Não fique para trás, permita que a{" "}
+            Não fique para trás, permita que a {"\n"}
             <span className="adnBold">ADN</span> te ajude a conquistar o mundo
             {"\n"}digital e alcançar o topo do mercado virtual!
           </p>
